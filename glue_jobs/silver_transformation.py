f02a21d016dbefeda74473181800a68454761bca
gowthamkethineni25 <gowthamkethineni008@gmail.com>
2026-09-02 23:34:40 -0400

    CI/CD deployment test

--- glue_jobs/silver_transformation.py
+++ glue_jobs/silver_transformation.py
@@ -249,6 +249,9 @@ def main():
 
         config = silver_config[table_name]
 
+        logger.info(
+            "Starting Silver transformation job - CI/CD deployment test")
+
         logger.info(f"Starting Silver transformation for {table_name}")
 
         bronze_df = read_bronze_table(config["bronze_path"])
